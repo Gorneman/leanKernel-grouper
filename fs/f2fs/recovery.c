6f793cacc5d4e8cba672d1e9e246a3afafcf254f
gorneman <gorneman@email.it>
2015-02-22 10:09:17 +0100

    f2fs: use meta_inode cache to improve roll-forward speed
    Previously, all the dnode pages should be read during the roll-forward recovery.
    Even worsely, whole the chain was traversed twice.
    This patch removes that redundant and costly read operations by using page cache
    of meta_inode and readahead function as well.
    
    Reviewed-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org> (reverted from commit 4249d149ec78bc6bb4dd4b9b957f31f72f21f642)

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -138,7 +138,7 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head)
 {
 	unsigned long long cp_ver = cur_cp_version(F2FS_CKPT(sbi));
 	struct curseg_info *curseg;
-	struct page *page = NULL;
+	struct page *page;
 	block_t blkaddr;
 	int err = 0;
 
@@ -146,14 +146,20 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head)
 	curseg = CURSEG_I(sbi, CURSEG_WARM_NODE);
 	blkaddr = NEXT_FREE_BLKADDR(sbi, curseg);
 
+	/* read node page */
+	page = alloc_page(GFP_F2FS_ZERO);
+	if (!page)
+		return -ENOMEM;
+	lock_page(page);
+
 	while (1) {
 		struct fsync_inode_entry *entry;
 
-		if (blkaddr < SM_I(sbi)->main_blkaddr ||
-			blkaddr >= (SM_I(sbi)->seg0_blkaddr + TOTAL_BLKS(sbi)))
-			return 0;
+		err = f2fs_submit_page_bio(sbi, page, blkaddr, READ_SYNC);
+		if (err)
+			return err;
 
-		page = get_meta_page_ra(sbi, blkaddr);
+		lock_page(page);
 
 		if (cp_ver != cpver_of_node(page))
 			break;
@@ -196,9 +202,11 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head)
 next:
 		/* check next segment */
 		blkaddr = next_blkaddr_of_node(page);
-		f2fs_put_page(page, 1);
 	}
-	f2fs_put_page(page, 1);
+
+	unlock_page(page);
+	__free_pages(page, 0);
+
 	return err;
 }
 
@@ -392,7 +400,7 @@ static int recover_data(struct f2fs_sb_info *sbi,
 {
 	unsigned long long cp_ver = cur_cp_version(F2FS_CKPT(sbi));
 	struct curseg_info *curseg;
-	struct page *page = NULL;
+	struct page *page;
 	int err = 0;
 	block_t blkaddr;
 
@@ -400,29 +408,32 @@ static int recover_data(struct f2fs_sb_info *sbi,
 	curseg = CURSEG_I(sbi, type);
 	blkaddr = NEXT_FREE_BLKADDR(sbi, curseg);
 
+	/* read node page */
+	page = alloc_page(GFP_F2FS_ZERO);
+	if (!page)
+		return -ENOMEM;
+
+	lock_page(page);
+
 	while (1) {
 		struct fsync_inode_entry *entry;
 
-		if (blkaddr < SM_I(sbi)->main_blkaddr ||
-			blkaddr >= (SM_I(sbi)->seg0_blkaddr + TOTAL_BLKS(sbi)))
-			break;
+		err = f2fs_submit_page_bio(sbi, page, blkaddr, READ_SYNC);
+		if (err)
+			return err;
 
-		page = get_meta_page_ra(sbi, blkaddr);
+		lock_page(page);
 
-		if (cp_ver != cpver_of_node(page)) {
-			f2fs_put_page(page, 1);
+		if (cp_ver != cpver_of_node(page))
 			break;
-		}
 
 		entry = get_fsync_inode(head, ino_of_node(page));
 		if (!entry)
 			goto next;
 
 		err = do_recover_data(sbi, entry->inode, page, blkaddr);
-		if (err) {
-			f2fs_put_page(page, 1);
+		if (err)
 			break;
-		}
 
 		if (entry->blkaddr == blkaddr) {
 			iput(entry->inode);
@@ -432,8 +443,11 @@ static int recover_data(struct f2fs_sb_info *sbi,
 next:
 		/* check next segment */
 		blkaddr = next_blkaddr_of_node(page);
-		f2fs_put_page(page, 1);
 	}
+
+	unlock_page(page);
+	__free_pages(page, 0);
+
 	if (!err)
 		allocate_new_segments(sbi);
 	return err;
@@ -478,10 +492,6 @@ int recover_fsync_data(struct f2fs_sb_info *sbi)
 out:
 	destroy_fsync_dnodes(&inode_list);
 	kmem_cache_destroy(fsync_entry_slab);
-
-	/* truncate meta pages to be used by the recovery */
-	truncate_inode_pages_range(META_MAPPING(sbi),
-		SM_I(sbi)->main_blkaddr << PAGE_CACHE_SHIFT, -1);
 
 	if (err) {
 		truncate_inode_pages(NODE_MAPPING(sbi), 0);
